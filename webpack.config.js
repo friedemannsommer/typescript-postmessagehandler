1b2114235327948ab1e0306224b2fd7a1d0bb9f1
Friedemann Sommer <social@friedemannsommer.com>
2021-03-30 21:08:59 +0200

    updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,50 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    target: 'web',
-    mode: 'production',
-    entry: {
-        index: path.resolve(__dirname, 'src/index.ts')
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        libraryTarget: 'umd',
-        libraryExport: 'default',
-        library: 'PostMessageHandler'
-    },
-    resolve: {
-        modules: [
-            'node_modules'
-        ],
-        extensions: ['.js', '.ts']
-    },
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'tslint-loader',
-                    options: {
-                        configFile: path.resolve(__dirname, './tslint.json'),
-                        emitErrors: true,
-                        failOnHint: true,
-                        typeCheck: true,
-                        fix: false
-                    }
-                }
+  target: "web",
+  mode: "production",
+  entry: {
+    index: path.resolve(__dirname, "src/index.ts"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    libraryTarget: "umd",
+    libraryExport: "default",
+    library: "PostMessageHandler",
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".js", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "tslint-loader",
+          options: {
+            configFile: path.resolve(__dirname, "./tslint.json"),
+            emitErrors: true,
+            failOnHint: true,
+            typeCheck: true,
+            fix: false,
+          },
+        },
+      },
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              reportFiles: ["./src/**/*.ts"],
             },
-            {
-                test: /\.ts$/,
-                use: [
-                    {
-                        loader: 'awesome-typescript-loader',
-                        options: {
-                            useCache: true,
-                            reportFiles: [
-                                './src/**/*.ts'
-                            ]
-                        }
-                    }
-                ]
-            }
-        ]
-    }
-}
+          },
+        ],
+      },
+    ],
+  },
+};
