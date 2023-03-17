d8dba6ba5c914b90ef7982c65d466cf4bd6a2fb3
Friedemann Sommer <social@friedemannsommer.com>
2023-03-17 14:09:40 +0100

    updated generated declarations

--- dist/lib/bind.d.ts
+++ dist/lib/bind.d.ts
@@ -1,2 +1,2 @@
-export declare type BindFn<T extends unknown[] = unknown[]> = (...args: T) => void;
+export type BindFn<T extends unknown[] = unknown[]> = (...args: T) => void;
 export default function bindFn<T extends unknown[] = unknown[]>(fn: BindFn<T>, context: object | undefined, ...givenArguments: unknown[]): BindFn<T>;
